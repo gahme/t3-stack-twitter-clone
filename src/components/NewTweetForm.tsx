a7658a7d9f8bf44d7cb13d76a6d65143823aafe2
gahme <g_ahmeddd@yahoo.com>
2023-09-02 16:09:16 +0300

    profile page also working

--- src/components/NewTweetForm.tsx
+++ src/components/NewTweetForm.tsx
@@ -18,6 +18,7 @@ function Form() {
         updateTextAreaHeight(textArea);
         textAreaRef.current = textArea;
     }, []);
+    const trpcUtils = api.useContext();
 
     useLayoutEffect(() => {
         updateTextAreaHeight(textAreaRef.current);
@@ -26,8 +27,33 @@ function Form() {
 
     const createTweet = api.tweet.create.useMutation({
         onSuccess: (newTweet) => {
-            console.log(newTweet);
             setInputValue("");
+            if (session.status !== "authenticated") return;
+
+            trpcUtils.tweet.infiniteFeed.setInfiniteData({}, (oldData) => {
+                if (oldData?.pages[0] == null) return
+
+                const newCacheTweet = {
+                    ...newTweet,
+                    likeCount: 0,
+                    likedByMe: false,
+                    user: {
+                        id: session.data.user.id,
+                        name: session.data.user.name ?? null,
+                        image: session.data.user.image ?? null,
+                    }
+                }
+
+                return {
+                    ...oldData,
+                    pages: [
+                        {
+                            ...oldData.pages[0],
+                            tweets: [newCacheTweet, ...oldData.pages[0].tweets]
+                        }
+                    ]
+                }
+            });
         },
         onError: (error) => {
             console.log(error);
